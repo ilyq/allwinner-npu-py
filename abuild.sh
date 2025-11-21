48955c15c2fcf34840640e79f934d9ee26ee8f20
ilyq <850246539@qq.com>
2025-11-22 00:00:56 +0800

    可以交叉编译了

--- abuild.sh
+++ abuild.sh
@@ -2,7 +2,19 @@
 
 set -e
 
+
+export ANDROID_NDK_HOME=/home/sd1/work/allwinner-npu-py/3rd_party/android-ndk-r25c
+
 rm -rf build
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DPYBIND11_NOPYTHON=ON
+# cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_SYSTEM_NAME=Android
+cmake -B build \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_SYSTEM_NAME=Android \
+  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+  -DANDROID_NDK=$ANDROID_NDK_HOME \
+  -DANDROID_ABI=arm64-v8a \
+  -DANDROID_PLATFORM=android-24
 cmake --build build
 
